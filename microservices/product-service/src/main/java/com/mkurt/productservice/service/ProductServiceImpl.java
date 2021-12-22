1c5b9a55222471b68c16108755ac9dd7c0062d6b
mkurt <info.mkurt@gmail.com>
2021-12-22 23:03:30 +0300

    Add mock data. closes #10

--- microservices/product-service/src/main/java/com/mkurt/productservice/service/ProductServiceImpl.java
+++ microservices/product-service/src/main/java/com/mkurt/productservice/service/ProductServiceImpl.java
@@ -2,6 +2,8 @@ package com.mkurt.productservice.service;
 
 import com.mkurt.api.core.product.Product;
 import com.mkurt.api.core.product.ProductService;
+import com.mkurt.api.exception.InvalidInputException;
+import com.mkurt.api.exception.NotFoundException;
 import com.mkurt.util.http.ServiceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -22,7 +24,17 @@ public class ProductServiceImpl implements ProductService {
 
     @Override
     public Product getProduct(int productId) {
-        return new Product();
+        LOG.debug("/product return the found product for productId={}", productId);
+
+        if (productId < 1) {
+            throw new InvalidInputException("Invalid productId: " + productId);
+        }
+
+        if (productId == 13) {
+            throw new NotFoundException("No product found for productId: " + productId);
+        }
+
+        return new Product(productId, "name-" + productId, 123, serviceUtil.getServiceAddress());
     }
 
 }

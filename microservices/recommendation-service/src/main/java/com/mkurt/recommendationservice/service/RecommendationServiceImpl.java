0862de3c8bee3600730ca9e3c9572dd2fd71bcab
mkurt <info.mkurt@gmail.com>
2021-12-23 20:50:37 +0300

    Add skeleton service for recommendation. closes #11

--- microservices/recommendation-service/src/main/java/com/mkurt/recommendationservice/service/RecommendationServiceImpl.java
+++ microservices/recommendation-service/src/main/java/com/mkurt/recommendationservice/service/RecommendationServiceImpl.java
@@ -2,11 +2,13 @@ package com.mkurt.recommendationservice.service;
 
 import com.mkurt.api.core.recommendation.Recommendation;
 import com.mkurt.api.core.recommendation.RecommendationService;
+import com.mkurt.api.exception.InvalidInputException;
 import com.mkurt.util.http.ServiceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,7 +25,23 @@ public class RecommendationServiceImpl implements RecommendationService {
 
     @Override
     public List<Recommendation> getRecommendations(int productId) {
-        return null;
+        if (productId < 1) {
+            throw new InvalidInputException("Invalid productId: " + productId);
+        }
+
+        if (productId == 113) {
+            LOG.debug("No recommendations found for productId: {}", productId);
+            return new ArrayList<>();
+        }
+
+        List<Recommendation> list = new ArrayList<>();
+        list.add(new Recommendation(productId, 1, "Author 1", 1, "Content 1", serviceUtil.getServiceAddress()));
+        list.add(new Recommendation(productId, 2, "Author 2", 2, "Content 2", serviceUtil.getServiceAddress()));
+        list.add(new Recommendation(productId, 3, "Author 3", 3, "Content 3", serviceUtil.getServiceAddress()));
+
+        LOG.debug("/recommendation response size: {}", list.size());
+
+        return list;
     }
 
 }

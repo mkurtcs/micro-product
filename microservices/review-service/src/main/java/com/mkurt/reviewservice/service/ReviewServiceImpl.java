4259b2898a324cdbefaae3aefa7de01ca87b86e2
mkurt <info.mkurt@gmail.com>
2021-12-23 20:50:05 +0300

    Add skeleton service for review. closes #12

--- microservices/review-service/src/main/java/com/mkurt/reviewservice/service/ReviewServiceImpl.java
+++ microservices/review-service/src/main/java/com/mkurt/reviewservice/service/ReviewServiceImpl.java
@@ -2,11 +2,13 @@ package com.mkurt.reviewservice.service;
 
 import com.mkurt.api.core.review.Review;
 import com.mkurt.api.core.review.ReviewService;
+import com.mkurt.api.exception.InvalidInputException;
 import com.mkurt.util.http.ServiceUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -23,7 +25,23 @@ public class ReviewServiceImpl implements ReviewService {
 
     @Override
     public List<Review> getReviews(int productId) {
-        return null;
+        if (productId < 1) {
+            throw new InvalidInputException("Invalid productId: " + productId);
+        }
+
+        if (productId == 213) {
+            LOG.debug("No reviews found for productId: {}", productId);
+            return new ArrayList<>();
+        }
+
+        List<Review> list = new ArrayList<>();
+        list.add(new Review(productId, 1, "Author 1", "Subject 1", "Content 1", serviceUtil.getServiceAddress()));
+        list.add(new Review(productId, 2, "Author 2", "Subject 2", "Content 2", serviceUtil.getServiceAddress()));
+        list.add(new Review(productId, 3, "Author 3", "Subject 3", "Content 3", serviceUtil.getServiceAddress()));
+
+        LOG.debug("/reviews response size: {}", list.size());
+
+        return list;
     }
 
 }

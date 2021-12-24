4711c01e6d933c99856d60734b5170384d0f030a
mkurt <info.mkurt@gmail.com>
2021-12-24 22:59:59 +0300

    Add recommendation repository. closes #16

---
+++ microservices/recommendation-service/src/main/java/com/mkurt/recommendationservice/persistence/RecommendationRepository.java
@@ -0,0 +1,12 @@
+package com.mkurt.recommendationservice.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface RecommendationRepository extends CrudRepository<RecommendationEntity, String> {
+
+    List<RecommendationEntity> findByProductId(int productId);
+}

cb6b85f609d2a86064f442f8a1b349dec9ec572e
mkurt <info.mkurt@gmail.com>
2021-12-29 00:35:41 +0300

    Add mongodb and mysql tests. closes #30

---
+++ microservices/product-service/src/test/java/com/mkurt/productservice/PersistenceTests.java
@@ -0,0 +1,154 @@
+package com.mkurt.productservice;
+
+import com.mkurt.productservice.persistence.ProductEntity;
+import com.mkurt.productservice.persistence.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+import static java.util.stream.IntStream.rangeClosed;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.data.domain.Sort.Direction.ASC;
+
+/**
+ * When using the @DataMongoTest and @DataJpaTest annotations instead of the @SpringBootTest annotation
+ * to only start up the MongoDB and SQL database during the integration test, there is one more thing to consider.
+ * The @DataMongoTest and @DataJpaTest annotations are designed to start an embedded database by default. Since we want
+ * to use a containerized database, we have to disable this feature. For the @DataMongoTest annotation, this can be done
+ * by using the excludeAutoConfiguration parameter and specifying that the class EmbeddedMongoAutoConfiguration will be excluded.
+ */
+@DataMongoTest(excludeAutoConfiguration = EmbeddedMongoAutoConfiguration.class)
+class PersistenceTests extends MongoDbTestBase {
+
+    @Autowired
+    private ProductRepository repository;
+
+    private ProductEntity savedEntity;
+
+    @BeforeEach
+    void setupDb() {
+        repository.deleteAll();
+
+        ProductEntity entity = new ProductEntity(1, "n", 1);
+        savedEntity = repository.save(entity);
+
+        assertEqualsProduct(entity, savedEntity);
+    }
+
+
+    @Test
+    void create() {
+
+        ProductEntity newEntity = new ProductEntity(2, "n", 2);
+        repository.save(newEntity);
+
+        ProductEntity foundEntity = repository.findById(newEntity.getId()).get();
+        assertEqualsProduct(newEntity, foundEntity);
+
+        assertEquals(2, repository.count());
+    }
+
+    @Test
+    void update() {
+        savedEntity.setName("n2");
+        repository.save(savedEntity);
+
+        ProductEntity foundEntity = repository.findById(savedEntity.getId()).get();
+        // when an entity is created, its version field is set to 0 by Spring Data, so we expect it to be 1 after the update.
+        assertEquals(1, (long)foundEntity.getVersion());
+        assertEquals("n2", foundEntity.getName());
+    }
+
+    @Test
+    void delete() {
+        repository.delete(savedEntity);
+        assertFalse(repository.existsById(savedEntity.getId()));
+    }
+
+    @Test
+    void getByProductId() {
+        Optional<ProductEntity> entity = repository.findByProductId(savedEntity.getProductId());
+
+        assertTrue(entity.isPresent());
+        assertEqualsProduct(savedEntity, entity.get());
+    }
+
+    @Test
+    void duplicateError() {
+        assertThrows(DuplicateKeyException.class, () -> {
+            ProductEntity entity = new ProductEntity(savedEntity.getProductId(), "n", 1);
+            repository.save(entity);
+        });
+    }
+
+    /**
+     * It is a test that verifies a correct error handling in the case of updates of stale data.
+     * It verifies that the optimistic locking mechanism works.
+     */
+    @Test
+    void optimisticLockError() {
+
+        // Store the saved entity in two separate entity objects
+        ProductEntity entity1 = repository.findById(savedEntity.getId()).get();
+        ProductEntity entity2 = repository.findById(savedEntity.getId()).get();
+
+        // Update the entity using the first entity object
+        entity1.setName("n1");
+        repository.save(entity1);
+
+        // Update the entity using the second entity object.
+        // This should fail since the second entity now holds an old version number, i.e. an Optimistic Lock Error
+        assertThrows(OptimisticLockingFailureException.class, () -> {
+            entity2.setName("n2");
+            repository.save(entity2);
+        });
+
+        // Get the updated entity from the database and verify its new sate
+        ProductEntity updatedEntity = repository.findById(savedEntity.getId()).get();
+        assertEquals(1, (int)updatedEntity.getVersion());
+        assertEquals("n1", updatedEntity.getName());
+    }
+
+    @Test
+    void paging() {
+
+        repository.deleteAll();
+
+        List<ProductEntity> newProducts = rangeClosed(1001, 1010)
+                .mapToObj(i -> new ProductEntity(i, "name " + i, i))
+                .collect(Collectors.toList());
+        repository.saveAll(newProducts);
+
+        Pageable nextPage = PageRequest.of(0, 4, ASC, "productId");
+        nextPage = testNextPage(nextPage, "[1001, 1002, 1003, 1004]", true);
+        nextPage = testNextPage(nextPage, "[1005, 1006, 1007, 1008]", true);
+        nextPage = testNextPage(nextPage, "[1009, 1010]", false);
+    }
+
+    private Pageable testNextPage(Pageable nextPage, String expectedProductIds, boolean expectsNextPage) {
+        Page<ProductEntity> productPage = repository.findAll(nextPage);
+        assertEquals(expectedProductIds, productPage.getContent().stream().map(p -> p.getProductId()).collect(Collectors.toList()).toString());
+        assertEquals(expectsNextPage, productPage.hasNext());
+        return productPage.nextPageable();
+    }
+
+    private void assertEqualsProduct(ProductEntity expectedEntity, ProductEntity actualEntity) {
+        assertEquals(expectedEntity.getId(),               actualEntity.getId());
+        assertEquals(expectedEntity.getVersion(),          actualEntity.getVersion());
+        assertEquals(expectedEntity.getProductId(),        actualEntity.getProductId());
+        assertEquals(expectedEntity.getName(),           actualEntity.getName());
+        assertEquals(expectedEntity.getWeight(),           actualEntity.getWeight());
+    }
+}
---
+++ microservices/review-service/src/test/java/com/mkurt/reviewservice/PersistenceTests.java
@@ -0,0 +1,131 @@
+package com.mkurt.reviewservice;
+
+import com.mkurt.reviewservice.persistence.ReviewEntity;
+import com.mkurt.reviewservice.persistence.ReviewRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.OptimisticLockingFailureException;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@DataJpaTest
+/** By default, Spring Boot configures the tests to roll back updates to the SQL database to minimize
+ * the risk of negative side effects on other tests. In our case, this behavior will cause some of the tests to fail.
+ * Therefore, automatic rollback is disabled with the class level annotation @Transactional(propagation = NOT_SUPPORTED) **/
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
+/**
+ * Finally, when using the @DataMongoTest and @DataJpaTest annotations instead of the @SpringBootTest annotation to only
+ * start up the MongoDB and SQL database during the integration test, there is one more thing to consider.
+ * The @DataMongoTest and @DataJpaTest annotations are designed to start an embedded database by default.
+ * Since we want to use a containerized database, we have to disable this feature. For the @DataJpaTest annotation,
+ * this can be done by using a @AutoConfigureTestDatabase annotation like this:
+ * **/
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+public class PersistenceTests extends MySqlTestBase {
+
+    @Autowired
+    private ReviewRepository repository;
+
+    private ReviewEntity savedEntity;
+
+    @BeforeEach
+    void setupDb() {
+        repository.deleteAll();
+
+        ReviewEntity entity = new ReviewEntity(1, 2, "a", "s", "c");
+        savedEntity = repository.save(entity);
+
+        assertEqualsReview(entity, savedEntity);
+    }
+
+
+    @Test
+    void create() {
+
+        ReviewEntity newEntity = new ReviewEntity(1, 3, "a", "s", "c");
+        repository.save(newEntity);
+
+        ReviewEntity foundEntity = repository.findById(newEntity.getId()).get();
+        assertEqualsReview(newEntity, foundEntity);
+
+        assertEquals(2, repository.count());
+    }
+
+    @Test
+    void update() {
+        savedEntity.setAuthor("a2");
+        repository.save(savedEntity);
+
+        ReviewEntity foundEntity = repository.findById(savedEntity.getId()).get();
+        assertEquals(1, (long)foundEntity.getVersion());
+        assertEquals("a2", foundEntity.getAuthor());
+    }
+
+    @Test
+    void delete() {
+        repository.delete(savedEntity);
+        assertFalse(repository.existsById(savedEntity.getId()));
+    }
+
+    @Test
+    void getByProductId() {
+        List<ReviewEntity> entityList = repository.findByProductId(savedEntity.getProductId());
+
+        assertThat(entityList, hasSize(1));
+        assertEqualsReview(savedEntity, entityList.get(0));
+    }
+
+    @Test
+    void duplicateError() {
+        assertThrows(DataIntegrityViolationException.class, () -> {
+            ReviewEntity entity = new ReviewEntity(1, 2, "a", "s", "c");
+            repository.save(entity);
+        });
+
+    }
+
+    @Test
+    void optimisticLockError() {
+
+        // Store the saved entity in two separate entity objects
+        ReviewEntity entity1 = repository.findById(savedEntity.getId()).get();
+        ReviewEntity entity2 = repository.findById(savedEntity.getId()).get();
+
+        // Update the entity using the first entity object
+        entity1.setAuthor("a1");
+        repository.save(entity1);
+
+        // Update the entity using the second entity object.
+        // This should fail since the second entity now holds an old version number, i.e. an Optimistic Lock Error
+        assertThrows(OptimisticLockingFailureException.class, () -> {
+            entity2.setAuthor("a2");
+            repository.save(entity2);
+        });
+
+        // Get the updated entity from the database and verify its new sate
+        ReviewEntity updatedEntity = repository.findById(savedEntity.getId()).get();
+        assertEquals(1, (int)updatedEntity.getVersion());
+        assertEquals("a1", updatedEntity.getAuthor());
+    }
+
+    private void assertEqualsReview(ReviewEntity expectedEntity, ReviewEntity actualEntity) {
+        assertEquals(expectedEntity.getId(),        actualEntity.getId());
+        assertEquals(expectedEntity.getVersion(),   actualEntity.getVersion());
+        assertEquals(expectedEntity.getProductId(), actualEntity.getProductId());
+        assertEquals(expectedEntity.getReviewId(),  actualEntity.getReviewId());
+        assertEquals(expectedEntity.getAuthor(),    actualEntity.getAuthor());
+        assertEquals(expectedEntity.getSubject(),   actualEntity.getSubject());
+        assertEquals(expectedEntity.getContent(),   actualEntity.getContent());
+    }
+}
